c45dc380dd29f5eeaf846f1d08af299af88bdd32
nontachai <pappaliving6395@gmail.com>
2025-02-10 00:31:34 +0700

    add netlify

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -3,12 +3,10 @@
 import { IProduct } from "@/type";
 import React, { createContext, useContext, useEffect, useState } from "react";
 
-// Define Cart Item Type
 export interface CartItem extends IProduct{
   quantity: number;
 }
 
-// Define Context Type
 interface CartContextType {
   cart: CartItem[];
   addToCart: (item: IProduct) => void;
@@ -18,25 +16,20 @@ interface CartContextType {
   clearCart: () => void;
 }
 
-// Create Context
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
-// Provider Component
 export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [cart, setCart] = useState<CartItem[]>([]);
 
-  // Load cart from localStorage on mount
   useEffect(() => {
     const savedCart = localStorage.getItem("cart");
     if (savedCart) setCart(JSON.parse(savedCart));
   }, []);
 
-  // Save cart to localStorage when cart changes
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  // Add Item to Cart
   const addToCart = (item: IProduct) => {
     setCart((prevCart) => {
       const existingItem = prevCart.find((cartItem) => cartItem.id === item.id);
@@ -63,17 +56,14 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
         .map((item) =>
           item.id === id ? { ...item, quantity: item.quantity - 1 } : item
         )
-        .filter((item) => item.quantity > 0) // Remove item if quantity is 0
+        .filter((item) => item.quantity > 0)
     );
   };
 
-
-  // Remove Item from Cart
   const removeFromCart = (id: number) => {
     setCart((prevCart) => prevCart.filter((item) => item.id !== id));
   };
 
-  // Clear Cart
   const clearCart = () => {
     setCart([]);
   };
@@ -85,7 +75,6 @@ export const CartProvider: React.FC<{ children: React.ReactNode }> = ({ children
   );
 };
 
-// Custom Hook to use Cart
 export const useCart = () => {
   const context = useContext(CartContext);
   if (!context) throw new Error("useCart must be used within a CartProvider");
